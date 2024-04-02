7c1f2c3344daf53cbd51dadbdb371a0a3e42da9c
guimotab <guimota22@gmail.com>
2024-04-02 12:19:04 -0400

    back: minor

---
+++ back/dist/js/routes/routes.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import authRoute from './authRoutes.js';
+import managerRoute from './managerRoutes.js';
+import ownerRoute from './ownerRoutes.js';
+import companyRoute from './companyRoutes.js';
+import storeRoute from './storeRoute.js';
+import flavorsRoute from './flavorsRoutes.js';
+import revenueRoute from './revenueRoutes.js';
+import addressRoute from './addressRoute.js';
+export const routes = (app) => {
+    app.get('/', (req, res) => {
+        res.status(200).json({ msg: "Bem-vindo à nossa API!" });
+    });
+    app.use(express.json(), authRoute, companyRoute, managerRoute, ownerRoute, storeRoute, addressRoute, stockRoute, flavorsRoute, revenueRoute);
+};
+//# sourceMappingURL=routes.js.map
