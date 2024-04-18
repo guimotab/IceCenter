e539b9435372d81518e46cd0032a7b7327f36460
guimotab <guimota22@gmail.com>
2024-04-18 07:17:14 -0400

    minor

--- back/src/service/IHttpService.ts
+++
@@ -1,7 +0,0 @@
-export interface IHttpService<T> {
-  getAll(): Promise<T[]>
-  get(id: string):Promise<T>
-  putData(id: string, data: T): void
-  postData(data: T): void
-  deleteData(id: string): void
-}
