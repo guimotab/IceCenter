56c52855fdd7fba9c69a1358b31c7daea626e216
guimotab <guimota22@gmail.com>
2024-07-25 14:05:00 -0400

    fix: erro vercel

--- app/api/address/[addressId]/route.ts
+++ app/api/address/[addressId]/route.ts
@@ -1,6 +1,6 @@
 
 import { IAddress } from "@/interface/IAddress";
-import { prisma } from "@/lib/prisma"
+import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
 interface IParamsProps {
